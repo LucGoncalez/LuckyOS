c7a1c49cbad29b3803bc113ef76856e793b13b23
Luciano Goncalez <master.lucky.br@gmail.com>
2016-08-29 00:29:01 -0300

    Adicionando system-v0.0

---
+++ kernel/src/system.pas
@@ -0,0 +1,47 @@
+{===========================================================================
+  Este arquivo pertence ao Projeto do Sistema Operacional LuckyOS (LOS).
+  --------------------------------------------------------------------------
+  Copyright (C) 2013 - Luciano L. Goncalez
+  --------------------------------------------------------------------------
+  a.k.a.: Master Lucky
+  eMail : master.lucky.br@gmail.com
+  Home  : http://lucky-labs.blogspot.com.br
+============================================================================
+  Este programa e software livre; voce pode redistribui-lo e/ou modifica-lo
+  sob os termos da Licenca Publica Geral GNU, conforme publicada pela Free
+  Software Foundation; na versao 2 da Licenca.
+
+  Este programa e distribuido na expectativa de ser util, mas SEM QUALQUER
+  GARANTIA; sem mesmo a garantia implicita de COMERCIALIZACAO ou de
+  ADEQUACAO A QUALQUER PROPOSITO EM PARTICULAR. Consulte a Licenca Publica
+  Geral GNU para obter mais detalhes.
+
+  Voce deve ter recebido uma copia da Licenca Publica Geral GNU junto com
+  este programa; se nao, escreva para a Free Software Foundation, Inc., 59
+  Temple Place, Suite 330, Boston, MA 02111-1307, USA. Ou acesse o site do
+  GNU e obtenha sua licenca: http://www.gnu.org/
+============================================================================
+  Unit System.pas
+  --------------------------------------------------------------------------
+  Unit principal do compilador, crosscompiler, que substitui a RTL normal.
+  --------------------------------------------------------------------------
+  Versao: 0.0
+  Data: 29/04/2013
+  --------------------------------------------------------------------------
+  Compilar: Compilavel FPC
+  > fpc system.pas
+  ------------------------------------------------------------------------
+  Executar: Nao executavel diretamente; Unit.
+===========================================================================}
+
+unit system;
+
+interface
+
+type
+  HResult = LongWord;
+  DWord = LongWord;
+
+implementation
+
+end.
